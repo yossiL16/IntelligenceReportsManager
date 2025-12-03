fc3632aa3d36dba6d74e133df8c00e4a21340c28
yossi lerer <ylerer770@gmail.com>
2025-12-03 17:45:33 +0200

    add num 2

---
+++ db/dataBase.js
@@ -0,0 +1,22 @@
+ let dataBase = [
+    {
+        id: "934",
+        terroristName: "yossi",
+        weapons: ["gun","bomb"],
+        text: "a"
+
+    },{
+        id: "456",
+        terroristName: "yossi",
+        weapons: ["gun","bomb","yod","kkkk"],
+        text: "z"
+
+    },{
+        id: "123",
+        terroristName: "yossi",
+        weapons: ["gun"],
+        text: "b"
+
+    }
+]
+export {dataBase}
